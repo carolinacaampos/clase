988242bc3a772fd63186d03bd408faba4ba4c5b4
carolinacaampos <38320197+carolinacaampos@users.noreply.github.com>
2018-06-14 12:22:43 -0300

    Update and rename .gitignore to simpson.pl

---
+++ .gitignore/simpson.pl
@@ -0,0 +1,28 @@
+
+
+% con un parametro
+
+% esTio(Alguien) :- hermanoConHijo(Alguien).
+% hermanoConHijo(Alguien) :- esHermano(Alguien,Hermano) , esPadre(Hermano,_).
+esHermano(Alguien,Otro) :- esPadre(Padre,Alguien) , esPadre(Padre,Otro).
+
+
+% con dos parametros
+
+esTio2(Alguien,Sobrino) :- hermanoConHijo(Alguien,Sobrino).
+hermanoConHijo(Alguien,Sobrino) :- esHermano(Alguien,Hermano) , esPadre(Hermano,Sobrino).
+
+esHijoUnico(Alguien) :- not(esHermano(Alguien,_))
+
+% hechos:
+esPadre(homero,bart).
+esPadre(homero,maggie).
+esPadre(ned,rod).
+esPadre(ned,tod).
+esPadre(homero, lisa).
+esPadre(abe,herbert).
+esPadre(abe, homero).
+esPadre(marge,maggie).
+esPadre(marge, lisa).
+esPadre(marge,bart).
+esHermano(bart,lisa).
